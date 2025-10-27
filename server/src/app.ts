349a2cfa4f99864ee1e962781b88e2b0ca11c073
Faiyaz Sarkar <faiyazvai999@gmail.com>
2025-10-27 12:02:46 +0600

    frontend URI is added to the cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -7,7 +7,12 @@ import listingRoutes from "./routes/listing.routes.js";
 import cookieParser from "cookie-parser";
 
 // Middleware
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://triphaven-faiyaz.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
